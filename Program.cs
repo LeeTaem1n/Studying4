1d088a3a92988b3d7e8f09381bab8146a3a85c67
danil <danil@DESKTOP-P63D37J>
2024-10-19 22:17:26 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace RemoveWhiteSpace
+{ 
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            char[] WhiteSpace = { ' ', '\n', '\t' };
+            string s = " this is a\nstring";
+            Console.WriteLine("Before:" + s );
+            Console.WriteLine("After:");
+            for (; ; )
+            {
+                int offset = s.IndexOfAny(WhiteSpace);
+                if (offset == -1)
+                {
+                    break;
+                }    
+                string before = s.Substring(0, offset);
+                string after = s.Substring(offset + 1);
+                s = string.Concat(before, after);
+            }
+            Console.WriteLine(s);
+            Console.WriteLine("Press 'Enter' to over");
+            Console.Read(); 
+        }
+    }
+}
